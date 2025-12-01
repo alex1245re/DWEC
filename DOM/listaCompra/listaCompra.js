5ff4e583a29c5bb13bc712fa616298c8eb8fcdee
alex <alejandrofelipediaz@gmail.com>
2025-12-01 10:31:07 +0100

    lista de la compra en js plano con localStorage y boton borrar, falta boton de editar

--- DOM/listaCompra/listaCompra.js
+++ DOM/listaCompra/listaCompra.js
@@ -1,29 +1,47 @@
-window.onload = ()=>
-{
-    var ul = document.getElementById("lista");
-    var opciones = document.getElementsByTagName("li");
-
+window.onload = ()=>{
+    
+    var lista = document.getElementById("lista");
     var mibtn = document.getElementById("btnGuardar");
+    var input = document.getElementById("input");
+
+
+    listaCompra = JSON.parse(localStorage.getItem("listaCompra"));
+
+    if(listaCompra){
+        for(alimento of listaCompra){
+            creaVista(alimento);
+        }
+    }
+    else
+        listaCompra = [];
+
 
     mibtn.addEventListener("click", ()=>{
-        let contenidoDelInput = document.getElementById("input").value;
+        
+        if(document.getElementById("input").value.trim() == "") return
 
-        let nuevoLI = document.createElement("li");
-        nuevoLI.innerHTML = contenidoDelInput;
-        if(contenidoDelInput == "") return;
-
-
-        ul.appendChild(nuevoLI);
-        btnBorrar = document.createElement("button");
-        btnBorrar.innerHTML = "🗑️";
-        nuevoLI.appendChild(btnBorrar);
-
-        document.getElementById("input").value = "";
+        listaCompra.push(document.getElementById("input").value);
+        creaVista(listaCompra[listaCompra.length-1]);
+        localStorage.setItem("listaCompra",JSON.stringify(listaCompra));
     });
+}
 
-    document.addEventListener("click",(e)=>{
-        if(e.target.tagName == "LI"){
-            e.target.remove();
-        }
+function creaVista(elemento){
+    
+    let nuevoLI = document.createElement("li");
+    nuevoLI.innerHTML = elemento;
+
+    lista.appendChild(nuevoLI);
+    btnBorrar = document.createElement("button");
+    btnBorrar.innerHTML = "🗑️";
+    nuevoLI.appendChild(btnBorrar);
+
+    document.getElementById("input").value = "";
+
+    btnBorrar.addEventListener("click",()=>{
+        nuevoLI.remove();
+        elementoBorrar = listaCompra.find((nuevoLI)=>nuevoLI);
+        listaCompra = listaCompra.filter(elemento => elemento != elementoBorrar);
+        localStorage.setItem("listaCompra",JSON.stringify(listaCompra));
     });
 }
